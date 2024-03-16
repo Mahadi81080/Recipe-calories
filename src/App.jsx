ff1438ae28e67f2fb6dd21082138922f42a3e8ac
Mahadi810803 <mahedihasan81080@gmail.com>
2024-03-16 12:32:05 +0600

    make header and banner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,131 @@
 import { useEffect } from "react";
 import "./App.css";
 import { useState } from "react";
+import { CiTimer } from "react-icons/ci";
+import { FaFire } from "react-icons/fa";
+import Header from "./Components/Header/Header";
+import Banner from "./Components/Banner/Banner";
 
 function App() {
   const [foods, setfoods] = useState([]);
+  const [cartItem, setCartItem] = useState([]);
   useEffect(() => {
     fetch("Recips.json")
       .then((res) => res.json())
       .then((data) => setfoods(data));
   }, []);
-  console.log(foods);
+  const handleAddCart = (food) => {
+    // console.log(food)
+    setCartItem([...cartItem, food]);
+  };
+  console.log(cartItem);
   return (
     <>
-      <section className="flex justify-between gap-5">
+    <Header></Header>
+    <Banner></Banner>
+      <section className="flex justify-between gap-8 mx-10">
         <div className="grid grid-cols-2 gap-4">
           {foods.map((food) => {
-            const{recipe_name,recipe_image,short_description,ingredients,preparing_time,calories}=food;
+            const {
+              recipe_name,
+              recipe_image,
+              short_description,
+              ingredients,
+              preparing_time,
+              calories,
+            } = food;
             return (
-              <div  key={food.id} className="card w-96 bg-base-100 shadow-xl">
+              <div key={food.id} className="card bg-base-100 shadow-xl">
                 <figure className="px-10 pt-10">
-                  <img
-                    src={recipe_image}
-                    alt="Recipe"
-                    className="rounded-xl"
-                  />
+                  <img src={recipe_image} alt="Recipe" className="rounded-xl" />
                 </figure>
                 <div className="card-body">
-                  <h2 className="card-title text-xl font-semibold">{recipe_name}</h2>
+                  <h2 className="card-title text-xl font-semibold">
+                    {recipe_name}
+                  </h2>
                   <p className="text-base font-normal">{short_description}</p>
-                  <div>
-                    <h3 className="text-lg font-medium">Ingredients : {food.ingredients.length}</h3>
-                    {
-                      ingredients.map(ingredient=>{console.log(ingredient)})
-                    }
+                  <hr />
+                  <div className="py-3 space-y-2">
+                    <h3 className="text-lg font-medium">
+                      Ingredients : {food.ingredients.length}
+                    </h3>
 
+                    {ingredients.map((ingredient) => {
+                      return (
+                        <li key={ingredient.id} className="ml-4">
+                          {ingredient}
+                        </li>
+                      );
+                    })}
                   </div>
-                  <div className="card-actions">
-                    <button className="btn bg-green-400 text-black rounded-full">Want to Cook</button>
+                  <div className="flex justify-between py-3">
+                    <div className="flex items-center gap-2">
+                      <span>
+                        <CiTimer />
+                      </span>
+                      <h2 className="text-base font-normal">30 minutes</h2>
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <span>
+                        <FaFire />
+                      </span>
+                      <h2 className="text-base font-normal">600 calories</h2>
+                    </div>
                   </div>
+                  <button
+                    onClick={() => handleAddCart(food)}
+                    className="btn bg-green-400 text-black rounded-full w-36"
+                  >
+                    Want to Cook
+                  </button>
                 </div>
               </div>
             );
           })}
         </div>
-        <div></div>
+
+        <div className="border p-3 rounded-xl">
+          <h1 className="text-2xl font-semibold text-center border-b border-gray-200 pb-3">
+            Want to cook: 01
+          </h1>
+          <div className="flex justify-center items-center gap-14 my-3 text-lg font-medium">
+            <h3></h3>
+            <h3>Name</h3>
+            <h3>time</h3>
+            <h3>Calories</h3>
+            <h3></h3>
+          </div>
+          {cartItem.map((cart, index) => {
+            const { recipe_name, preparing_time, calories } = cart;
+            return (
+              <div key={cart.id} className="bg-base-200 flex justify-center items-center gap-5 p-4 rounded-xl mb-3 text-base font-normal">
+                <h5>{index + 1}</h5>
+                <h5>{recipe_name}</h5>
+                <h5>{preparing_time}</h5>
+                <h5>{calories}</h5>
+                <button className="btn bg-green-400 text-black rounded-full w-28 text-base">
+                  Preparing
+                </button>
+              </div>
+            );
+          })}
+          {/* <div className="overflow-x-auto">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Name</th>
+                  <th>Time</th>
+                  <th>Calories</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                
+              </tbody>
+            </table>
+          </div> */}
+        </div>
       </section>
     </>
   );
---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,32 @@
+const Banner = () => {
+  return (
+    <section className="mx-10 mb-5">
+      <div className="bg-[url('https://i.postimg.cc/cCkxJcg7/Rectangle-1.jpg')] bg-cover bg-center h-[500px] text-center flex justify-center items-center rounded-xl">
+        <div className="space-y-5">
+          <h3 className="text-5xl font-gramond font-extrabold text-white">
+            Discover an exceptional cooking <br /> class tailored for you!
+          </h3>
+          <p className="font-poppins text-white w-2/3 mx-auto text-lg">
+            Learn and Master Basic Programming, Data Structures, Algorithm, OOP,
+            Database and solve 500+ coding problems to become an exceptionally
+            well world-class Programmer.
+          </p>
+          <button className="btn bg-green-400 text-black rounded-full text-base px-8 mr-5">
+          Explore Now
+          </button>
+          <button className="btn btn-outline text-white rounded-full px-8">Our Feedback</button>
+        </div>
+      </div>
+      <div className="py-16 text-center space-y-2">
+          <h3 className="text-4xl text-black font-extrabold font-gramond">
+          Our Recipes
+          </h3>
+          <p className="max-w-3xl mx-auto font-poppins">
+          Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus vulputate netus pharetra rhoncus. Eget urna volutpat curabitur elementum mauris aenean neque. 
+          </p>
+        </div>
+    </section>
+  );
+};
+
+export default Banner;
